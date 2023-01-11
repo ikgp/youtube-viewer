bc4ca1ba67e9afb55dc4083283fd19290a9e0ec7
Aaron Dewes <aaron.dewes@protonmail.com>
2023-01-11 13:32:53 +0100

    Test blocked page

--- window.cpp
+++ window.cpp
@@ -14,7 +14,7 @@ void Window::init() {
     QVBoxLayout *layout = new QVBoxLayout;
     QWebEngineView *webView = new QWebEngineView(this);
     this->m_webView = webView;
-    this->m_webView->setUrl(QUrl("https://youtube.com"));
+    this->m_webView->setUrl(QUrl("https://www.eff.org/deeplinks/2015/02/who-really-owns-your-drones"));
     layout->setSpacing(0);
     layout->setMargin(0);
     layout->addWidget(webView);
